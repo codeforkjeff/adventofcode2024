a9d85b2b180147caa1aeb4675052210d30b38885
codeforkjeff <jeff@codefork.com>
2024-12-01 12:03:14 -0800

    use heap (PriorityQueue) instead of sorting ArrayList; rewrite in a more functional style

--- src/main/java/com/codefork/aoc2024/Day01Puzzle01.java
+++ src/main/java/com/codefork/aoc2024/Day01Puzzle01.java
@@ -1,30 +1,34 @@
 package com.codefork.aoc2024;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 public class Day01Puzzle01 extends Problem {
 
     @Override
     public String solve() {
-        var lists = new ArrayList<ArrayList<Integer>>();
-        lists.add(new ArrayList<Integer>());
-        lists.add(new ArrayList<Integer>());
+        // transform input into a list of heaps, one for each "column" in the input file
+        var lists = getInput().reduce(new ArrayList<PriorityQueue<Integer>>(),
+                (acc, line) -> {
+                    var parts = line.split("\\s+");
+                    for (var i = 0; i < parts.length; i++) {
+                        if (i >= acc.size()) {
+                            acc.add(new PriorityQueue<Integer>());
+                        }
+                        acc.get(i).add(Integer.valueOf(parts[i]));
+                    }
+                    return acc;
+                },
+                (acc1, acc2) -> {
+                    acc1.addAll(acc2);
+                    return acc1;
+                });
 
-        getInput().forEach(line -> {
-            var parts = line.split("\\s+");
-            for(var i =0; i < parts.length; i++) {
-                lists.get(i).add(Integer.valueOf(parts[i]));
-            }
-        });
-
-        Collections.sort(lists.get(0));
-        Collections.sort(lists.get(1));
-
-        var totalDistance = 0;
-        for(var i = 0; i < lists.get(0).size(); i++) {
-            totalDistance += Math.abs(lists.get(1).get(i) - lists.get(0).get(i));
-        }
+        var totalDistance = IntStream
+                .range(0, lists.get(0).size()).reduce(0, (acc, i) ->
+                        acc + Math.abs(lists.get(1).remove() - lists.get(0).remove())
+                );
 
         return String.valueOf(totalDistance);
     }
