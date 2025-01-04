41430603ca4dec6518abc1dad86d30d2599d6fcd
codeforkjeff <jeff@codefork.com>
2025-01-03 22:10:31 -0800

    cleanup

--- src/main/java/com/codefork/aoc2024/day21/PressSequence.java
+++ src/main/java/com/codefork/aoc2024/day21/PressSequence.java
@@ -1,8 +1,6 @@
 package com.codefork.aoc2024.day21;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,53 +32,6 @@ public record PressSequence(Map<Move, Long> moves) {
         return new PressSequence(moves);
     }
 
-//    /**
-//     * returns list of new PressSequences that are the possibilities that can result from adding the passed-in move
-//     *
-//     * @param keypad used to get the Button objects for the symbols, and to get the possible paths between buttons.
-//     */
-//    public List<PressSequence> addMove(Keypad keypad, Move move, long multiplier) {
-//        var results = new ArrayList<PressSequence>();
-//        var paths = keypad.getMovesToPaths().get(move);
-//
-//        //System.out.println("from=" + _from + " to=" + _to + " paths=" + paths);
-//
-//        for (var path : paths) {
-//            var copy = copy();
-//
-//            char first = path.charAt(0);
-//
-//            // note that we use dirKeypad here, since movements match up with the directional keypad
-//
-//            // add a transitional move from the last implicit "A"
-//            if (path.length() > 1 && first != 'A') {
-//                var transition = new Move(Keypad.dirKeypad.getButton("A"), Keypad.dirKeypad.getButton(first));
-//                copy.moves().merge(transition, multiplier, Long::sum);
-//            }
-//
-//            var from = first;
-//            for (var i = 1; i < path.length(); i++) {
-//                var to = path.charAt(i);
-//
-//                var moveInPath = new Move(Keypad.dirKeypad.getButton(from), Keypad.dirKeypad.getButton(to));
-//                copy.moves().merge(moveInPath, multiplier, Long::sum);
-//
-//                from = to;
-//            }
-//
-//            // this happens when a button repeats (i.e. moves from a button to itself)
-//            if (path.length() == 1) {
-//                var aButton = Keypad.dirKeypad.getButton("A");
-//                var moveInPath = new Move(aButton, aButton);
-//                copy.moves().merge(moveInPath, multiplier, Long::sum);
-//            }
-//            results.add(copy);
-//        }
-//        //System.out.println("addMove returning=" + results);
-//
-//        return results;
-//    }
-//
     public PressSequence copy() {
         return new PressSequence(new HashMap<>(moves));
     }
